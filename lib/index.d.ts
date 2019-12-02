614334fa35b37f8e0c2d6716225c6e4e7165d1e6
Bobby Nijjar <bobby951@gmail.com>
2019-12-01 22:17:50 -0500

    stop ignoring build files

---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import ace, { IAceEditorProps } from "./ace";
+import { IAceOptions, IAnnotation, ICommand, ICommandBindKey, ICommandManager, IEditorProps, IMarker } from "./types";
+export { IAceOptions, IAceEditorProps, IAnnotation, ICommand, ICommandBindKey, ICommandManager, IEditorProps, IMarker };
+export default ace;
