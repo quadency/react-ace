314743ee057a277f129e97468d2f076ee4cd81b6
Bobby Nijjar <bobby951@gmail.com>
2019-12-01 22:14:53 -0500

    removed split and diff editors to decrease bundle size

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,4 @@
 import ace, { IAceEditorProps } from "./ace";
-import diff, { IDiffEditorProps, IDiffEditorState } from "./diff";
-import split, { ISplitEditorProps } from "./split";
 import {
   IAceOptions,
   IAnnotation,
@@ -11,18 +9,13 @@ import {
   IMarker
 } from "./types";
 export {
-  split,
-  diff,
   IAceOptions,
   IAceEditorProps,
   IAnnotation,
   ICommand,
   ICommandBindKey,
   ICommandManager,
-  IDiffEditorProps,
-  IDiffEditorState,
   IEditorProps,
-  IMarker,
-  ISplitEditorProps
+  IMarker
 };
 export default ace;
